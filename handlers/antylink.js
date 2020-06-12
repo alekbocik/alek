9d91dde0c31a272e4776d9581cfbb73dfed217e1
alekbocik <63149934+alekbocik@users.noreply.github.com>
2020-06-12 03:13:30 +0200

    Add files via upload

--- handlers/antylink.js
+++ handlers/antylink.js
@@ -1,57 +1,85 @@
 const Discord = require("discord.js")
 const { bot } = require('../storemc.js');
 
-bot.on('messageUpdate', function(oldMessage, newMessage) {
+bot.on('messageUpdate', async function(oldMessage, newMessage) {
   if(oldMessage.author.bot) return;
   if(oldMessage.channel.type === "dm") return;
+  if(newMessage.channel.parentID == "720774619212021781") return;
   let embed = new Discord.MessageEmbed()
-      .setAuthor(`${oldMessage.author.username}` + " " + "Edytowanie wiadomośći na linki jest również zabronione", newMessage.author.displayAvatarURL)
-      .setColor("#c21bb9")
+  .setColor("#c21bb9")
+  .setAuthor(oldMessage.author.username, oldMessage.author.avatarURL({ dynamic:true }))
+  .setThumbnail('https://imgur.com/sF6PLGa.png')
+  .setDescription("Edytowanie wiadomośći na linki jest również zabronione")
   const bannedWords = [`https://`, `http://`, `www.`, `.pl`, `.com`, `.eu`, `discord.gg`, `.gg/`, `.gg /`, `. gg /`, `. gg/`, `discord .gg /`, `discord.gg /`, `discord .gg/`, `discord .gg`, `discord . gg`, `discord. gg`, `discord gg`, `discordgg`, `discord gg /`]
-  try {
       if (bannedWords.some(word => newMessage.content.toLowerCase().includes(word))) {
         if(!oldMessage.member.roles.cache.some(r=>["ROOT","perm.link", "BOT"].includes(r.name))) {
-          return oldMessage.delete().catch(O_o=>{}) + newMessage.channel.send(embed).then(msg => msg.delete({ timeout: 30000 }));
-      }
-      }
-  } catch (e) {
-      console.log(e);
-  }
+          try {
+            await newMessage.delete()
+          } catch(err) {
+            let embed = new Discord.MessageEmbed()
+            .setColor("#c21bb9")
+            .setAuthor(newMessage.author.username, newMessage.author.avatarURL({ dynamic:true }))
+            .setThumbnail('https://imgur.com/sF6PLGa.png')
+            .setDescription("Brak permisji aby usunąc link w wiadomosci")
+            newMessage.channel.send(embed).then(msg => msg.delete({ timeout: 30000 }));
+            return;
+          }
+          newMessage.channel.send(embed).then(msg => msg.delete({ timeout: 30000 }));
+          newMessage.delete().catch(O_o=>{})
+        }
+    }
 });
 bot.on("message", async message => {
   if(message.author.bot) return;
   if(message.channel.type === "dm") return;
+  if(message.channel.parentID == "720774619212021781") return; 
   const antylink = [`https://`, `http://`, `www.`, `.pl`, `.com`, `.eu`, `discord.gg`, `.gg/`, `.gg /`, `. gg /`, `. gg/`, `discord .gg /`, `discord.gg /`, `discord .gg/`, `discord .gg`, `discord . gg`, `discord. gg`, `discord gg`, `discordgg`, `discord gg /`]
   if(antylink.some(word => message.member.displayName.includes(word))){
     let xd = message.member.displayName
     let xd3 = xd.replace(`.pl`, "").replace(`www.`, "").replace(`https://`, "").replace(`.com`, "").replace(`.eu`, "")
-    if(!message.member.roles.cache.some(r=>["ROOT", "perm.link"].includes(r.name))) {
-      try{
-        let embed = new Discord.MessageEmbed()
+    if(!message.member.roles.cache.some(r=>["ROOT", "perm.link"].includes(r.name))) {        let embed = new Discord.MessageEmbed()
         .setAuthor(`${message.author.username}` + " " + "Linki w nicku są zabronione", message.author.displayAvatarURL)
         .setColor("#c21bb9")
+        try {
+          await message.member.setNickname(xd3)
+        } catch(err) {
+          let embed = new Discord.MessageEmbed()
+          .setColor("#c21bb9")
+          .setAuthor(message.author.username, message.author.avatarURL({ dynamic:true }))
+          .setThumbnail('https://imgur.com/sF6PLGa.png')
+          .setDescription("Brak permisji aby usunąc link z nicku ")
+          message.channel.send(embed).then(msg => msg.delete({ timeout: 30000 }));
+          return;
+        }
         message.channel.send(embed).then(msg => msg.delete({ timeout: 30000 }));
         message.delete().catch(O_o=>{})
-        message.member.setNickname(xd3)
-      } catch (e){
-        let embed = new Discord.MessageEmbed()
-        .setAuthor(`Brak permisji na usunięcie linku z nicku ${message.author.username}`)
-        .setColor("#c21bb9")
-        message.channel.send(embed).then(msg => msg.delete({ timeout: 30000 }));
       }
   }
-}
 })
 bot.on("message", async message => {
   if(message.author.bot) return;
   if(message.channel.type === "dm") return;
+  if(message.channel.parentID == "720774619212021781") return;
   const antylink = [`https://`, `http://`, `www.`, `.pl`, `.com`, `.eu`, `discord.gg`, `.gg/`, `.gg /`, `. gg /`, `. gg/`, `discord .gg /`, `discord.gg /`, `discord .gg/`, `discord .gg`, `discord . gg`, `discord. gg`, `discord gg`, `discordgg`, `discord gg /`]
   let embed = new Discord.MessageEmbed()
-      .setAuthor(`${message.author.username}` + " " + "Wysyłanie linków jest zabronione!!", message.author.displayAvatarURL)
-      .setColor("#c21bb9")
+  .setColor("#c21bb9")
+  .setAuthor(message.author.username, message.author.avatarURL({ dynamic:true }))
+  .setThumbnail('https://imgur.com/sF6PLGa.png')
+  .setDescription("Wysyłanie linków jest zabronione")
       if (antylink.some(word => message.content.toLowerCase().includes(word))) {
         if(!message.member.roles.cache.some(r=>["ROOT","perm.link", "BOT"].includes(r.name))) {
-          return message.delete().catch(O_o=>{}) + message.channel.send(embed).then(msg => msg.delete({ timeout: 30000 }));
+          try {
+            await message.delete()
+          } catch(err) {
+            let embed = new Discord.MessageEmbed()
+            .setColor("#c21bb9")
+            .setAuthor(message.author.username, message.author.avatarURL({ dynamic:true }))
+            .setThumbnail('https://imgur.com/sF6PLGa.png')
+            .setDescription("Brak permisji aby usunąc link w wiadomosci")
+            message.channel.send(embed).then(msg => msg.delete({ timeout: 30000 }));
+            return;
+        }
+        message.channel.send(embed).then(msg => msg.delete({ timeout: 30000 }));
       }
     }
   })
--- storemc.js
+++ storemc.js
@@ -1,19 +1,26 @@
 const discord = require('discord.js')
 const Gamedig = require('gamedig');
 const fs = require("fs")
-var mysql = require("mysql");
 const bot = new discord.Client({disableEveryone: false});
 bot.commands = new discord.Collection();
 bot.aliases = new discord.Collection();
 
-var connection = mysql.createConnection({
-  host: "remotemysql.com",
-  user: "A3VUvloKP8",
-  port: "3306",
-  password: "6u03PB4dcr",
-  database: "A3VUvloKP8"
-  });
-
+async function zmien(state){
+  const zmien = await bot.channels.cache.get("704097045157249215")
+  try {
+    await zmien.setName(`Online:` + " " + state.players.length + "/" + state.maxplayers)
+  } catch (error) {
+    return;
+  }
+}
+async function zmien2(state){
+  const zmien = await bot.channels.cache.get("704097045157249215")
+  try {
+    await zmien.setName(`Online: OFFLINE`)
+  } catch (error) {
+    return;
+  }
+}
 
 async function xd(){
   Gamedig.query({
@@ -21,13 +28,9 @@ async function xd(){
     port: "19132",
     host: 'storymc.pl'
   }).then((state) => {
-    connection.query(`UPDATE storymc SET mute = 1 WHERE userID = "287975660872400898"`)
-    const well = bot.channels.cache.get("704097045157249215")
-    well.setName(`Online:` + " " + state.players.length + "/" + state.maxplayers)
+    zmien(state)
 }).catch((error) => {
-  connection.query(`UPDATE storymc SET mute = 1 WHERE userID = "287975660872400898"`)
-  const well = bot.channels.cache.get("704097045157249215")
-  well.setName(`Online: OFFLINE`)
+  zmien2(state)
 });
 }
 async function xd2(){
@@ -36,115 +39,12 @@ async function xd2(){
     port: "19132",
     host: 'storymc.pl'
   }).then((state) => {
-    connection.query(`UPDATE storymc SET mute = 0 WHERE userID = "287975660872400898"`)
-    const well = bot.channels.cache.get("704097045157249215")
-    well.setName(`Online:` + " " + state.players.length + "/" + state.maxplayers)
+      zmien(state)
 }).catch((error) => {
-  connection.query(`UPDATE storymc SET mute = 0 WHERE userID = "287975660872400898"`)
-  const well = bot.channels.cache.get("704097045157249215")
-  well.setName(`Online: OFFLINE`)
+  zmien2(state)
 });
 }
 
-bot.on("message", async message => {
-  try {
-    if(message.author.bot) return;
-    if(message.channel.type === "dm") return;
-    let messageArray = message.content.toLowerCase().split(" ");
-    let cmd = messageArray[0];
-    let args = messageArray.slice(1);
-    if(message.content.startsWith("!mute")) {
-      if(!message.member.roles.cache.some(r=>["ROOT","perm.mute"].includes(r.name)))
-      return message.delete().catch(O_o=>{}) 
-      let user = message.guild.member(message.mentions.members.first());
-      if(!user) return message.delete().catch(O_o=>{})
-      if(user.id === "287975660872400898" || user.id === "397118855979466753" || user.id === "390196446613471236") return message.delete().catch(O_o=>{});
-    let sql = 'SELECT mute FROM storymc WHERE userID = ?';
-    connection.query(sql, [user.id] ,(err, results) => {
-      if(err) throw err;
-      if(results.length === 0){
-        connection.query(`INSERT INTO storymc (userID, mute) VALUES ('${user.id}', '1')`)
-        setInterval(() => {
-          let sql = 'SELECT * FROM storymc WHERE userID = ?';
-          connection.query(sql, [user.id], (err, results) => {
-            let user1 = results[0].mute
-            if(user1 === '0'){
-              return false
-            }
-            if(user1 === '1'){
-              message.guild.member(user).voice.setMute(true)
-              return false
-            }
-          })
-        }, 2000);
-      }
-      if(results.length === 1){
-        connection.query(`UPDATE storymc SET mute = 1 WHERE userID = ${user.id}`)
-        setInterval(() => {
-          let sql = 'SELECT * FROM storymc WHERE userID = ?';
-          connection.query(sql, [user.id], (err, results) => {
-            let user1 = results[0].mute
-            if(user1 === '0'){
-              return false
-            }
-            if(user1 === '1'){
-              message.guild.member(user).voice.setMute(true)
-              return false
-            }
-          })
-        }, 2000);
-      }
-    })
-}
-
-    if(message.content.startsWith("!rangamute")){
-      if(!message.author.id === "287975660872400898") return message.delete().catch(O_o=>{})
-      let user = message.guild.member(message.mentions.members.first());
-      if(!user) return message.delete().catch(O_o=>{})
-      let muterole = message.guild.roles.cache.find(role => role.name === 'perm.mute');
-      if(!muterole){
-        try{
-          muterole = await message.guild.roles.create({
-            data: {
-              name: 'perm.mute',
-              color: 'BLUE',
-            },
-          })
-        }catch(e){
-          console.log(e.stack);
-        }
-      return;
-    }
-    user.roles.add(muterole.id) + message.delete().catch(O_o=>{});
-    }
-    if(message.content.startsWith("!unmute")){
-      if(!message.member.roles.cache.some(r=>["ROOT","perm.mute"].includes(r.name)))
-      return message.delete().catch(O_o=>{}) 
-      let user = message.guild.member(message.mentions.members.first());
-      if(!user) return message.delete().catch(O_o=>{})
-      let sql = 'SELECT mute FROM storymc WHERE userID = ?';
-      connection.query(sql, [user.id], (err, results) => {
-        let user1 = results[0].mute
-        if(results.length === 0){
-          connection.query(`INSERT INTO storymc (userID, mute) VALUES ('${user.id}', '0')`)
-          return false
-        }
-        if(results.length === 1){
-          if(user1 === '0'){
-            return false
-          }
-          if(user1 === '1'){
-            connection.query(`UPDATE storymc SET mute = 0 WHERE userID = ${user.id}`)
-            message.guild.member(user).voice.setMute(false)
-            return false
-          }
-        }
-      })
-    }
-}catch (error) {
-  console.log(error)
-}
-})
 bot.on("ready", async () => {
   try {
     console.log(`${bot.user.username} online`)
@@ -158,7 +58,7 @@ setInterval(function() {
           xd2()
         status = 0;
       }
-    }, 20000);
+    }, 10000);
 }catch (e) {
   console.log(e);
 }
@@ -180,4 +80,3 @@ module.exports = {
   bot: bot
 };
 bot.login(process.env.token)
-connection.connect()
